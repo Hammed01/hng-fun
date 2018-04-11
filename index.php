f9c5ee6b7e490f4be26cc5ead51d59e96d64798d
hammed01 <shotolahammed01@gmail.com>
2018-04-11 22:51:21 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Portfolio</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    header{
+    background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("photo.jpg");
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    height:100vh;
+    position: relative;
+}
+
+p,small{
+    color: #fff;
+}
+
+div p{
+    position:absolute;
+    right: 34%;
+    bottom:10%;
+    font-size: 20px
+}
+
+ small{
+    position: absolute;
+     right:3%;
+     bottom: 3%;
+}
+    
+    
+    
+    </style>
+
+</head>
+    
+<header>
+    
+        <div>
+            <p> Hello I'm Oluwole,A Software developer from Ogun state </p>
+            <small>10:25:14pm
+            </small>
+       </div>
+           
+    
+    </header>
